6bed37e0105a6fb07b2e3376dadace709e89f495
LeathanTeal <leathan@gmail.com>
2017-12-28 13:29:03 -0800

    Add authentication middleware to routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,25 +1,26 @@
 const todosController = require('../controllers').todos;
 const todoItemsController = require('../controllers').todoItems;
+const authCheck = require('../auth.js');
 
 module.exports = (app) => {
-  app.get('/api', (req, res) => res.status(200).send({
+  app.get('/api', authCheck, (req, res) => res.status(200).send({
     message: 'Welcome to the Todos API!',
   }));
 
-  app.post('/api/todos', todosController.create);
-  app.get('/api/todos', todosController.list);
-  app.get('/api/todos/:todoId', todosController.retrieve);
-  app.put('/api/todos/:todoId', todosController.update);
-  app.delete('/api/todos/:todoId', todosController.destroy);
+  app.post('/api/todos', authCheck, todosController.create);
+  app.get('/api/todos', authCheck, todosController.list);
+  app.get('/api/todos/:todoId', authCheck, todosController.retrieve);
+  app.put('/api/todos/:todoId', authCheck, todosController.update);
+  app.delete('/api/todos/:todoId', authCheck, todosController.destroy);
 
-  app.post('/api/todos/:todoId/items', todoItemsController.create);
-  app.put('/api/todos/:todoId/items/:todoItemId', todoItemsController.update);
+  app.post('/api/todos/:todoId/items', authCheck, todoItemsController.create);
+  app.put('/api/todos/:todoId/items/:todoItemId', authCheck, todoItemsController.update);
   app.delete(
-    '/api/todos/:todoId/items/:todoItemId', todoItemsController.destroy
+    '/api/todos/:todoId/items/:todoItemId', authCheck, todoItemsController.destroy
   );
 
   // For any other request method on todo items, we're going to return "Method Not Allowed"
-  app.all('/api/todos/:todoId/items', (req, res) =>
+  app.all('/api/todos/:todoId/items', authCheck, (req, res) =>
     res.status(405).send({
       message: 'Method Not Allowed',
   }));
