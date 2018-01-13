0bfd517d7889d3274b55f746940469cb75188886
LeathanTeal <leathan@gmail.com>
2018-01-13 14:53:37 -0800

    Change datatype of body to TEXT (for longer than 255 characters)

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -13,8 +13,6 @@ module.exports = {
       })
       .then(post => res.status(201).send(post))
       .catch(error => res.status(400).send(error));
-      console.log('**********HERE**********');
-      console.log(post); //
   },
   list(req, res) {
     return Post
--- server/migrations/20171229032331-create-post.js
+++ server/migrations/20171229032331-create-post.js
@@ -16,7 +16,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       body: {
-        type: Sequelize.STRING
+        type: Sequelize.TEXT
       },
       authorId: {
         type: Sequelize.STRING,
